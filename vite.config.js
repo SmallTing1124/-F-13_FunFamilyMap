5d6c05e5e2000f513ac82617d053716362b0048a
SmallTing1124 <chentt1124@gmail.com>
2024-09-20 11:58:19 +0800

    新增bootstrap+版型樣式

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/web-layout-training-vite/',
+  base: '/F-13_FunFamilyMap/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
