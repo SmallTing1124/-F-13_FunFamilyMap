d492d66b9736daa5c47c2816dad4474b98fabd30
SmallTing1124 <chentt1124@gmail.com>
2024-09-24 16:27:58 +0800

    新增首頁＋導覽頁

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 import './assets/scss/all.scss';
+import 'bootstrap'
 
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper-col-3', {
+  slidesPerView: 1,
+  breakpoints: {
+    991: {
+      slidesPerView: 3,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    }
+  },
+  
+});
 console.log("Hello world!");
