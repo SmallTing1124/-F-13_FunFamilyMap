6e20ced0dcecc836486b74d7094d22625613e4b7
wangdddw <mrha9145@gmail.com>
2024-10-31 16:35:42 +0800

    修改景點列表swiper

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-import Swiper from 'swiper';
-import { Grid, Navigation, Pagination } from 'swiper/modules';
+import Swiper from "swiper";
+import { Grid, Navigation, Pagination } from "swiper/modules";
 
 // import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/bundle';
+import "swiper/css";
+import "swiper/css/bundle";
 
 // Init Swiper for swiper-col-3
-const swiperCol3 = new Swiper('.swiper-col-3', {
-  modules: [Navigation],  // 設置使用的模塊
+const swiperCol3 = new Swiper(".swiper-col-3", {
+  modules: [Navigation], // 設置使用的模塊
   slidesPerView: 1,
   breakpoints: {
     991: {
@@ -16,16 +16,16 @@ const swiperCol3 = new Swiper('.swiper-col-3', {
         nextEl: ".swiper-col-3-button-next",
         prevEl: ".swiper-col-3-button-prev",
       },
-    }
+    },
   },
 });
 
 const swiperGrid = new Swiper(".swiper-grid", {
-  modules: [Navigation, Pagination, Grid],  // 設置使用的模塊
-  slidesPerView: 1.2,  // Mobile default
+  modules: [Navigation, Pagination, Grid], // 設置使用的模塊
+  slidesPerView: 1.2, // Mobile default
   spaceBetween: 24,
-  loop:true,
-  centeredSlides:true,
+  loop: true,
+  centeredSlides: true,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
@@ -41,13 +41,23 @@ const swiperGrid = new Swiper(".swiper-grid", {
     992: {
       slidesPerView: 3,
       grid: {
-        rows: 2,  // 使用 'rows' 而不是 'row'
-        fill: 'row',
+        rows: 2, // 使用 'rows' 而不是 'row'
+        fill: "row",
       },
-    }
-  }
+    },
+  },
 });
-
+const touristSwiper = new Swiper(".tourist-img-swiper", {
+  modules: [Navigation, Pagination],
+  pagination: {
+    el: ".tourist-img-swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-col-3-button-next",
+    prevEl: ".swiper-col-3-button-prev",
+  },
+});
 
 //電腦版 喜愛景點、想去景點、打卡按鈕
 let isFavoriteClicked = false;
@@ -60,7 +70,7 @@ if (isFavoriteClicked === false) {
   pcBtnCheck1.addEventListener("click", function (message, type) {
     console.log("pcbtn1click");
     isFavoriteClicked = !isFavoriteClicked;
-
+    console.log(isFavoriteClicked);
     // console.log(isFavoriteClicked);
   });
 } else if (isFavoriteClicked === true) {
@@ -69,13 +79,32 @@ if (isFavoriteClicked === false) {
     isFavoriteClicked = !isFavoriteClicked;
   });
 }
-pcBtnCheck2.addEventListener("click", function () {
-  console.log("pcbtn2click");
-  console.log(isFavoriteClicked);
-});
-pcBtnCheck3.addEventListener("click", function () {
-  console.log("pcbtn3click");
-});
+if (isWantToGoClicked === false) {
+  pcBtnCheck2.addEventListener("click", function (message, type) {
+    console.log("pcbtn2click");
+    isWantToGoClicked = !isWantToGoClicked;
+    console.log(isWantToGoClicked);
+    // console.log(isWantToGoClicked);
+  });
+} else if (isWantToGoClicked === true) {
+  pcBtnCheck2.addEventListener("click", function () {
+    console.log("pcbtn2NoClick");
+    isWantToGoClicked = !isWantToGoClicked;
+  });
+}
+if (isCheckClicked === false) {
+  pcBtnCheck3.addEventListener("click", function (message, type) {
+    console.log("pcbtn3click");
+    isCheckClicked = !isCheckClicked;
+    console.log(isCheckClicked);
+    // console.log(isCheckClicked);
+  });
+} else if (isCheckClicked === true) {
+  pcBtnCheck3.addEventListener("click", function () {
+    console.log("pcbtn3NoClick");
+    isCheckClicked = !isCheckClicked;
+  });
+}
 //手機版 喜愛景點、想去景點、打卡按鈕
 const mbBtnCheck1 = document.getElementById("mb-btn-check");
 const mbBtnCheck2 = document.getElementById("mb-btn-check-2");
